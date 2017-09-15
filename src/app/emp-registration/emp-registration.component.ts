3abb3d5ee9c5b98e03a2ab848cc8100f5d577522
Godlyn Paul <godlynpaul@gmail.com>
2017-09-15 21:08:29 +0530

    Template driven forms

---
+++ src/app/emp-registration/emp-registration.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+	selector: 'app-emp-registration',
+	templateUrl: './emp-registration.component.html',
+	styleUrls: ['./emp-registration.component.css']
+})
+export class EmpRegistrationComponent implements OnInit {
+	constructor() {}
+	ngOnInit() {}
+
+	regValidation(formControl){
+		console.log(formControl);
+	}
+	empRegFormSave(formGroup){
+		console.log(formGroup)
+		console.log("Form value accessed using the formGroup: "+ formGroup.value.empName);
+	}
+
+}
