adc8c2d7739137455efa5ffa1e1307e3ef564342
Godlyn Paul <godlyn_paul@apple.com>
2017-10-17 10:49:12 +0530

    custom Validation but facing the access issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EmpRegistrationComponent } from './emp-registration/emp-registration.component';
 import { ContractEmpRegistrationComponent } from './contract-emp-registration/contract-emp-registration.component';
+// import { MobileNumberValidators } from './contract-emp-registration/mobilenumber-validators'
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		EmpRegistrationComponent,
 		ContractEmpRegistrationComponent
+		// MobileNumberValidators
 	],
 	imports: [
 		BrowserModule,
--- src/app/contract-emp-registration/contract-emp-registration.component.ts
+++ src/app/contract-emp-registration/contract-emp-registration.component.ts
@@ -14,7 +14,9 @@ export class ContractEmpRegistrationComponent implements OnInit {
   contactEmployee = new FormGroup({
     conEmpName : new FormControl('',Validators.required),
     conEmpSector : new FormControl('',[Validators.required, Validators.minLength(3)]),
-    conCompany : new FormControl('',[Validators.required, Validators.minLength(3)])
+    conCompany : new FormControl('',[Validators.required, Validators.minLength(3)]),
+    // conEmpMobile : new FormControl('',MobileNumberValidators.cannotContainMinus)
+    conEmpMobile : new FormControl('',Validators.required)
   });
   get gConEmpName(){
     return this.contactEmployee.get('conEmpName');
@@ -22,4 +24,7 @@ export class ContractEmpRegistrationComponent implements OnInit {
   get gConCompany(){
     return this.contactEmployee.get('conCompany');
   };
+  get gConEmpMobile(){
+    return this.contactEmployee.get('conEmpMobile');
+  }
 }
